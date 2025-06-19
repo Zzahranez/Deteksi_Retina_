f074040f408c56b5e7c1495fe6e068f236dd2991
Zzahranez <zahranezaldi123@gmail.com>
2025-06-19 09:25:57 +0700

    revisi

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+import tensorflow as tf
+from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return render_template('deteksi.html')
+model = tf.keras.models.load_model(r"D:\Data Sains dan Analitis\Pertemuan 16 Project Akhir\Deteksi_retina\retina_v2.h5")
+
+class_labels = ['Retina Normal', 'Retinopathy Retina',]  # Sesuaikan dengan 
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'Tidak ada file gambar'})
+    
+    file = request.files['file']
+    img = Image.open(file).resize((128, 128))
+    img_array = np.array(img) / 255.
+    img_array = np.expand_dims(img_array, axis=0)
+
+
+    # Prediksi gambar menggunakan model
+    prediction = model.predict(img_array)
+    print("Nilai Prediksi:", prediction)
+
+    # Tentukan kelas berdasarkan nilai prediksi
+    if prediction[0][0] > 0.5:
+        predicted_class = 'Retinopathy Retina'
+    elif prediction[0][0] <= 0.5:
+        predicted_class = 'Normal Retina'
+    else:
+        predicted_class = 'Retina tidak diketahui'
+
+    print("Kelas yang diprediksi:", predicted_class)
+    # Buat respons yang lebih terstruktur
+    hasil_prediksi = {
+        'diagnosis': predicted_class,
+        'confidence': float(prediction[0][0]) 
+    }
+
+    return jsonify(hasil_prediksi)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Auto-reload saat ada perubahan kode
+    app.run(debug=True)
